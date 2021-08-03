e67503c74fcc3d12099bc3ced5b3a207e36637c0
Cainã Muniz <adrianmuniz.ceo@outlook.com>
2021-08-02 22:22:58 -0300

    Resolvendo o exercicio 08 com While

---
+++ src/Ex08.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Ex08 {
+
+	public static void main(String[] args) {
+		//Um professor quer saber quantos alunos de uma sala de 50 tiveram nota maior do que a média. Faça um programa onde o professor informe a média e as notas de cada um dos 50 alunos e, ao final, seja exibido quantos alunos tiveram nota superior à média e quantos tiveram nota inferior à média.
+		
+		Scanner leitor = new Scanner (System.in);
+		double media, nota = 0.0;
+		int qtde = 1;
+		int aprovados = 0;
+		
+		System.out.println("Qual a media da turma? ");
+		media = leitor.nextDouble();
+		
+		while (qtde <= 50) {
+			System.out.println("Informe a nota do º" + qtde + " aluno:");
+			nota = leitor.nextDouble();
+			if (nota > media) {
+				aprovados = aprovados + 1;
+			}
+			qtde = qtde + 1;
+		}
+		
+		System.out.println("Alunos acima da média: " + aprovados);
+	}
+
+}
